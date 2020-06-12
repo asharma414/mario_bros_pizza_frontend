cb2a36bd509ad06ba88c37567c47f18ac733afe2
asharma414 <abhi.sharm.414@gmail.com>
2020-06-12 10:58:48 -0400

    basic layout setup

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const mtoDiv = () => document.querySelector('#mto')
+const specialsDiv = () => document.querySelector('#specials')
+const loginDiv = () => document.querySelector('#login')
+const containerDiv = () => document.querySelector('.container')
+
+document.addEventListener('DOMContentLoaded', () => {
+    mtoDiv().addEventListener('click', renderMTO)
+    specialsDiv().addEventListener('click', renderSpecials)
+    loginDiv().addEventListener('click', renderLogin)
+})
+
+const renderMTO = () => {
+
+}
+
+const renderSpecials = () => {
+    
+}
+
+const renderLogin = () => {
+
+}
